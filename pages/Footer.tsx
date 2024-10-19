0b3fc4fa09bcb7392dc6753c4b5a61fb3fe4f36d
michaelwcsin <michael.wc.sin@gmail.com>
2024-10-18 21:00:25 -0600

    First commit, added basic structure with assets

---
+++ pages/Footer.tsx
@@ -0,0 +1,12 @@
+import Socials from "@/components/socials";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <Socials />
+    </div>
+  );
+};
+
+export default Footer;
---
+++ pages/Header.tsx
@@ -0,0 +1,8 @@
+import IconLogo from "@/components/icons/logo";
+import React from "react";
+
+const Header = () => {
+  return <IconLogo />;
+};
+
+export default Header;
