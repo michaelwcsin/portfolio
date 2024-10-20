545c162e04e8186b3f2bfb6aff523290042b8916
michaelwcsin <michael.wc.sin@gmail.com>
2024-10-20 12:50:07 -0600

    Changed width on construction notice

--- pages/Projects.tsx
+++ pages/Projects.tsx
@@ -13,7 +13,7 @@ const Projects = () => {
   return (
     <div className="flex h-screen w-full items-center">
       <div className="flex-col w-[40vw] items-center gap-[10vw] m-[7vw]">
-        <p className="font-bold hidden md:block text-[1vw] w-[30vw] text-center mb-10">
+        <p className="font-bold hidden md:block text-[1vw] w-full text-center mb-10">
           Hello, this portfolio, more importantly this section of projects is
           currently under construction as of Oct 20, 2024. If you&apos;d like to
           know more or would like a live demo. Please contact me or you may
