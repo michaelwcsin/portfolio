1f67a2dfbdd1d2f73ed7cd93fd4da86af933a96c
michaelwcsin <michael.wc.sin@gmail.com>
2024-10-20 21:33:32 -0600

    Changed socials in footer

---
+++ components/link-reveal.tsx
@@ -0,0 +1,106 @@
+import { motion } from "framer-motion";
+import React from "react";
+
+export const RevealLinks = () => {
+  return (
+    <section className="flex flex-col gap-6 px-0 py-24 text-white">
+      <FlipLink
+        href="mailto:michael.wc.sin@gmail.com"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        Mail
+      </FlipLink>
+      <FlipLink
+        href="https://github.com/michaelwcsin"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        Github
+      </FlipLink>
+      <FlipLink
+        href="https://www.linkedin.com/in/mwsin/"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        Linkedin
+      </FlipLink>
+    </section>
+  );
+};
+
+const DURATION = 0.25;
+const STAGGER = 0.025;
+
+const FlipLink = ({
+  children,
+  href,
+  rel,
+  target,
+}: {
+  children: string;
+  href: string;
+  rel: string;
+  target: string;
+}) => {
+  return (
+    <motion.a
+      initial="initial"
+      whileHover="hovered"
+      href={href}
+      rel={rel}
+      target={target}
+      className="relative max-w-[50vw] md:max-w-[40vw] lg:max-w-[35vw] overflow-hidden whitespace-nowrap text-4xl font-black uppercase sm:text-5xl md:text-6xl lg:text-7xl"
+      style={{
+        lineHeight: 0.75,
+      }}
+    >
+      <div>
+        {children.split("").map((l, i) => (
+          <motion.span
+            variants={{
+              initial: {
+                y: 0,
+              },
+              hovered: {
+                y: "-100%",
+              },
+            }}
+            transition={{
+              duration: DURATION,
+              ease: "easeInOut",
+              delay: STAGGER * i,
+            }}
+            className="inline-block"
+            key={i}
+          >
+            {l}
+          </motion.span>
+        ))}
+      </div>
+      <div className="absolute inset-0">
+        {children.split("").map((l, i) => (
+          <motion.span
+            variants={{
+              initial: {
+                y: "100%",
+              },
+              hovered: {
+                y: 0,
+              },
+            }}
+            transition={{
+              duration: DURATION,
+              ease: "easeInOut",
+              delay: STAGGER * i,
+            }}
+            className="inline-block"
+            key={i}
+          >
+            {l}
+          </motion.span>
+        ))}
+      </div>
+    </motion.a>
+  );
+};
--- pages/Content.tsx
+++ pages/Content.tsx
@@ -1,61 +1,22 @@
-import Image from "next/image";
+import { RevealLinks } from "@/components/link-reveal";
 import React from "react";
 
 export default function Content() {
   return (
     <div className="bg-[#6050DC] py-8 px-12 h-full w-full flex flex-col justify-between">
-      <Section1 />
+      <RevealLinks />
       <Section2 />
     </div>
   );
 }
 
-const Section1 = () => {
-  return (
-    <div>
-      <Nav />
-    </div>
-  );
-};
-
 const Section2 = () => {
   return (
     <div className="flex justify-between items-end">
-      <h1 className="text-[8vw] leading-[.9] mt-10 font-bold tracking-tighter">
+      <h1 className="text-[8vw] leading-[.9] mt-10 font-black tracking-tight">
         Connect with me.
       </h1>
       <p className="text-sm md:mb-2 text-white">Copyright © Michael Sin</p>
-    </div>
-  );
-};
-
-const Nav = () => {
-  return (
-    <div className="flex shrink-0 gap-20">
-      <div className="flex text-[24px] md:text-[32px] font-semibold flex-col gap-4 text-white">
-        <a
-          href="https://github.com/michaelwcsin"
-          target="_blank"
-          rel="noreferrer noopener"
-          className="flex gap-2"
-        >
-          <Image src="iconGithub.svg" width={32} height={32} alt="github" />
-          <p>Github</p>
-        </a>
-        <a
-          href="https://www.linkedin.com/in/mwsin/"
-          target="_blank"
-          rel="noreferrer noopener"
-          className="flex gap-2"
-        >
-          <Image src="iconLinkedin.svg" width={32} height={32} alt="linkedin" />
-          <p>LinkedIn</p>
-        </a>
-        <a href="mailto:michael.wc.sin@gmail.com" className="flex gap-2">
-          <Image src="iconMail.svg" width={32} height={32} alt="mail" />
-          <p>Mail</p>
-        </a>
-      </div>
     </div>
   );
 };
